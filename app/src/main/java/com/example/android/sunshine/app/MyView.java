dcb8587e3ff3ffd57b69bd7ea2284f6184c14914
Eugene Guzik <guzik.evgeny@gmail.com>
2015-11-26 17:16:03 +0200

    5.19

---
+++ app/src/main/java/com/example/android/sunshine/app/MyView.java
@@ -0,0 +1,21 @@
+package com.example.android.sunshine.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Eugene on 26.11.2015.
+ */
+public class MyView extends View {
+    public MyView(Context context){
+        super(context);
+    }
+    public MyView(Context context, AttributeSet attrs, int defaultStyle){
+        super(context, attrs, defaultStyle);
+    }
+    @Override
+    protected void onMeasure(int wMeasureSpec, int hMeasureSpec){
+
+    }
+}
